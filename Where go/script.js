f3cf622f66626aa14ce1fc44b768d96848f79686
Gabriel Camargo Alves <140025786+Camargasso@users.noreply.github.com>
2023-11-15 12:49:05 -0300

    Add files via upload

--- Where go/script.js
+++ Where go/script.js
@@ -9,34 +9,6 @@ menuToggle.addEventListener("click", () => {
 closeMenuButton.addEventListener("click", () => {
     mobileMenu.classList.remove("open");
 });
-//----------------------------------------------------------
-const cardContainer = document.querySelector(".card-container");
-let scrollAmount = 0;
-
-function handleCarousel(direction) {
-    const cardWidth = 320; // Largura de cada card (ajuste conforme necessário)
-    scrollAmount += direction * cardWidth;
-    if (scrollAmount < 0) scrollAmount = 0;
-    if (scrollAmount > cardContainer.scrollWidth - cardContainer.clientWidth) {
-        scrollAmount = cardContainer.scrollWidth - cardContainer.clientWidth;
-    }
-    cardContainer.style.transform = `translateX(-${scrollAmount}px)`;
-}
-
-// Botões para controlar o carrossel
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
-
-prevButton.addEventListener("click", () => {
-    handleCarousel(-1);
-});
-
-nextButton.addEventListener("click", () => {
-    handleCarousel(1);
-});
-
-var largura = screen.width;
-var altura = screen.height;
 
 console.log("Largura da tela: " + largura + "px");
 console.log("Altura da tela: " + altura + "px");
